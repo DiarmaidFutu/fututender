859d75bda28d3a08d9d6cf72ea5f7a0f4a4821a8
Diarmaid <97042383+DiarmaidFutu@users.noreply.github.com>
2025-01-23 16:13:23 +0100

    feature/recurring-task (#3)
    * FutuTender: Add background task to run imports

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import asyncio
+import traceback
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
@@ -7,15 +9,31 @@ from db.database_session_manager import sessionmanager
 from db.utils import run_migrations
 from exceptions.base_exception import FutuTenderBaseException
 from exceptions.database_exception import FutuTenderDatabaseException
+from importer.importer import tenderize_and_save
 from routers.tenders import tender_router
+
+background_tasks = {}
+
+
+async def recurring_import():
+    while True:
+        try:
+            async with sessionmanager.session():
+                await tenderize_and_save()
+                await asyncio.sleep(3600)
+        except Exception as e:
+            print(e)
+            print(traceback.format_exc())
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     await run_migrations()
+    background_tasks["import"] = asyncio.create_task(recurring_import())
     yield
     if await sessionmanager.is_initialised():
         await sessionmanager.close()
+    background_tasks.get("import").cancel()
 
 
 app = FastAPI(lifespan=lifespan)
